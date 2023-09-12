1886b5af76f2274297c67e0378dda743357b73d1
lbringerl <lbringerl@hotmail.com>
2023-09-13 01:29:49 +0300

    test

--- api_test.py
+++ api_test.py
@@ -1,3 +1,5 @@
+import requests
+
 import openai
 
 
@@ -6,8 +8,18 @@ with open('api_key.key') as f:
 
 
 def main():
-    pass
-
+    openai.api_key = API_KEY
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {
+                'role': 'user',
+                'content': 'Multiply every equation by 2. What is the result of equation: 1*30'
+            }
+        ]
+    )
+    print(response)
+    
 
 if __name__ == '__main__':
     main()
